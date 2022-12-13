1f4b17f9bc6f75a7b71187e29cea85c4d300067e
Evans kiplagat <evansrotichh7@gmail.com>
2022-12-13 14:53:14 +0300

    added example plus clean up

--- Examples/UiToolkit/UI/HyperionExamplePanel.cs
+++ Examples/UiToolkit/UI/HyperionExamplePanel.cs
@@ -43,63 +43,37 @@ public class HyperionExamplePanel : MonoBehaviour
     private Label _blockTransactionMrootLabel;
     private Label _blockProducerSignatureLabel;
 
-
-
-    private Label _headBlockIdLabel;
+    //private Label _headBlockIdLabel;
     private Label _blockCPULimitLabel;
     private Label _blockNETLimitLabel;
     private Label _chainIdLabel;
-    private Label _ForkDbHeadBlockIdLabel;
-    private Label _ForkDbHeadBlockNumLabel;
-    private Label _HeadBlockIdLabel;
-    private Label _HeadBlockNumLabel;
-    private Label _HeadBlockProducerLabel;
-    private Label _HeadBlockTimeLabel;
+    private Label _forkDbHeadBlockIdLabel;
+    private Label _forkDbHeadBlockNumLabel;
+    private Label _headBlockIdLabel;
+    private Label _headBlockNumLabel;
+    private Label _headBlockProducerLabel;
+    private Label _headBlockTimeLabel;
 
-    private Label _collectionNameLabel;
-    private Label _ownerLabel;
     private Label _filterTypeLabel;
-    private Label _sellerLabel;
-    private Label _tradeOfferIdLabel;
-    private Label _nftNameLabel;
-    private Label _headerLabel;
-    private Label _idLabel;
-    private Label _priceLabel;
-    private Label _mintNumberLabel;
-    private Label _backedTokenLabel;
-    private Label _schemaNameLabel;
-    private Label _templateIdLabel;
-    private Label _propertiesTransferableLabel;
-    private Label _propertiesBurnableLabel;
 
     private Button _searchButton;
     private Button _accountButton;
     private Button _infoButton;
     private Button _blockButton;
 
-    private TextField _collectionNameOrAssetId;
+    private TextField _textFieldValue;
 
 
     /*
      * Fields/Properties
      */
 
-    private StatsClient _statsClient;
     private AccountsClient _accountsClient;
     private ChainClient _chainClient;
-
-    private string _createdAccount = "kingcoolcorv";
-    private string _blockNumber = "100000000";
-
 
     void Start()
     {
         Root = GetComponent<UIDocument>().rootVisualElement;
-
-        _statsClient = new StatsClient(new UnityWebRequestHandler())
-        {
-            BaseUrl = "https://wax.eosphere.io/"
-        };
 
         _accountsClient = new AccountsClient(new UnityWebRequestHandler())
         {
@@ -111,7 +85,7 @@ public class HyperionExamplePanel : MonoBehaviour
             BaseUrl = "https://wax.eosphere.io/"
         };
 
-        _collectionNameOrAssetId = Root.Q<TextField>("collection-name-or-id-textfield");
+        _textFieldValue = Root.Q<TextField>("collection-name-or-id-textfield");
 
         _accountLabel = Root.Q<Label>("account-name-label");
         _creatorLabel = Root.Q<Label>("creator-label");
@@ -133,38 +107,27 @@ public class HyperionExamplePanel : MonoBehaviour
         _blockCPULimitLabel = Root.Q<Label>("block-cpu-limit-label");
         _blockNETLimitLabel = Root.Q<Label>("block-net-limit-label");
         _chainIdLabel = Root.Q<Label>("chain-id-label");
-        _ForkDbHeadBlockIdLabel = Root.Q<Label>("fork-block-id-label");
-        _ForkDbHeadBlockNumLabel = Root.Q<Label>("fork-block-number-label");
-        _HeadBlockIdLabel = Root.Q<Label>("head-block-id-label");
-        _HeadBlockNumLabel = Root.Q<Label>("head-block-number-label");
-        _HeadBlockProducerLabel = Root.Q<Label>("head-block-producer-label");
-        _HeadBlockTimeLabel = Root.Q<Label>("head-block-time-label");
+        _forkDbHeadBlockIdLabel = Root.Q<Label>("fork-block-id-label");
+        _forkDbHeadBlockNumLabel = Root.Q<Label>("fork-block-number-label");
+        _headBlockIdLabel = Root.Q<Label>("head-block-id-label");
+        _headBlockNumLabel = Root.Q<Label>("head-block-number-label");
+        _headBlockProducerLabel = Root.Q<Label>("head-block-producer-label");
+        _headBlockTimeLabel = Root.Q<Label>("head-block-time-label");
 
 
         _accountBox = Root.Q<VisualElement>("account-box");
         _blockBox = Root.Q<VisualElement>("block-box");
         _infoBox = Root.Q<VisualElement>("info-box");
 
-        _headerLabel = Root.Q<Label>("header-label");
-        _nftNameLabel = Root.Q<Label>("nft-name-label");
-        _idLabel = Root.Q<Label>("id-label");
-        _tradeOfferIdLabel = Root.Q<Label>("trade-offer-id-label");
         _filterTypeLabel = Root.Q<Label>("filter-type");
-        _priceLabel = Root.Q<Label>("price-label");
-        _ownerLabel = Root.Q<Label>("owner-label");
-        _sellerLabel = Root.Q<Label>("seller-label");
-        _mintNumberLabel = Root.Q<Label>("mint-number-label");
-        _backedTokenLabel = Root.Q<Label>("backed-token-label");
-        _schemaNameLabel = Root.Q<Label>("schema-label");
-        _templateIdLabel = Root.Q<Label>("template-id-label");
-        _collectionNameLabel = Root.Q<Label>("collection-name-label");
-        _propertiesTransferableLabel = Root.Q<Label>("properties-transferable-label");
-        _propertiesBurnableLabel = Root.Q<Label>("properties-burnable-label");
 
         _searchButton = Root.Q<Button>("search-button");
         _accountButton = Root.Q<Button>("account-button");
         _infoButton = Root.Q<Button>("info-button");
         _blockButton = Root.Q<Button>("block-button");
+
+
+        _filterTypeLabel.text = "Account";
 
         BindButtons();
     }
@@ -228,66 +191,52 @@ public class HyperionExamplePanel : MonoBehaviour
 
     private void Rebind(GetInfoResponse info)
     {
-
-        //Assert.IsNotNull(info.ServerFullVersionString);
-        //Assert.IsNotNull(info.ServerVersion);
-        //Assert.IsNotNull(info.ServerVersionString);
-
-
-
         _headBlockIdLabel.text = info.HeadBlockId;
         _blockCPULimitLabel.text = info.BlockCpuLimit.ToString();
         _blockNETLimitLabel.text = info.BlockNetLimit.ToString();
         _chainIdLabel.text = info.ChainId;
-        _ForkDbHeadBlockIdLabel.text = info.ForkDbHeadBlockId;
-        _ForkDbHeadBlockNumLabel.text = info.ForkDbHeadBlockNum.ToString();
+        _forkDbHeadBlockIdLabel.text = info.ForkDbHeadBlockId;
+        _forkDbHeadBlockNumLabel.text = info.ForkDbHeadBlockNum.ToString();
         _headBlockIdLabel.text = info.HeadBlockId;
-        _HeadBlockNumLabel.text = info.HeadBlockNum.ToString();
-        _HeadBlockProducerLabel.text = info.HeadBlockProducer;
-        _HeadBlockTimeLabel.text = info.HeadBlockTime;
-
+        _headBlockNumLabel.text = info.HeadBlockNum.ToString();
+        _headBlockProducerLabel.text = info.HeadBlockProducer;
+        _headBlockTimeLabel.text = info.HeadBlockTime;
     }
-
-
 
     #endregion
 
     #region Others
-
     private async void SearchAsset()
     {
-        if (_filterTypeLabel.text != null)
+        try
         {
-            try
+            switch (_filterTypeLabel.text)
             {
-                switch (_filterTypeLabel.text)
-                {
-                    case "Account":
-                        var account = await _accountsClient.GetCreatorAsync(_createdAccount);
-                        if (account != null)
-                        {
-                            Rebind(account);
-                        }
-                        else Debug.Log("account not found");
-                        break;
+                case "Account":
+                    var account = await _accountsClient.GetCreatorAsync(_textFieldValue.value);
+                    if (account != null)
+                    {
+                        Rebind(account);
+                    }
+                    else Debug.Log("account not found");
+                    break;
 
-                    case "Block":
-                        var block = await _chainClient.GetBlockAsync(_blockNumber);
-                        if (block != null)
-                        {
-                            Rebind(block);
-                        }
-                        else Debug.Log("block not found");
-                        break;
+                case "Block":
+                    var block = await _chainClient.GetBlockAsync(_textFieldValue.value);
+                    if (block != null)
+                    {
+                        Rebind(block);
+                    }
+                    else Debug.Log("block not found");
+                    break;
 
-                    case "":
-                        break;
-                }
+                case "":
+                    break;
             }
-            catch (ApiException ex)
-            {
-                Debug.LogError($"Content: {ex.Content}");
-            }
+        }
+        catch (ApiException ex)
+        {
+            Debug.LogError($"Content: {ex.Content}");
         }
     }
     #endregion
